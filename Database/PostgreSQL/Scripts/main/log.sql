ed85893eb32fa28b68115737ea2cef367dd10961
nikolatonkev <nikola_tonkev@yahoo.com>
2013-11-01 18:56:21 -0400

    taskdestinationrelation table creted

--- Database/PostgreSQL/Scripts/main/log.sql
+++ Database/PostgreSQL/Scripts/main/log.sql
@@ -11,3 +11,23 @@ CREATE TABLE main.log
   systemstatusid            INT             NOT NULL,
   transactionid             INT             NOT NULL
 );
+
+ALTER TABLE main.log
+ADD CONSTRAINT Fk_main_Log_ApplicatioId FOREIGN KEY (applicationid)
+REFERENCES auth.application (applicationId) MATCH SIMPLE;
+
+ALTER TABLE main.log
+ADD CONSTRAINT Fk_main_Log_StatusId FOREIGN KEY (statusid)
+REFERENCES dict.logstatus (logstatusid) MATCH SIMPLE;
+
+ALTER TABLE main.log
+ADD CONSTRAINT Fk_main_Log_SystemStatusId FOREIGN KEY (systemstatusid)
+REFERENCES dict.systemstatus (systemstatusid) MATCH SIMPLE;
+
+ALTER TABLE main.log
+ADD CONSTRAINT Fk_main_Log_TaskDestinationRelationId FOREIGN KEY (taskdestinationrelationid)
+REFERENCES main.taskdestinationrelation (taskdestinationrelationid) MATCH SIMPLE;
+
+ALTER TABLE main.log
+ADD CONSTRAINT Fk_main_Log_TransactionId FOREIGN KEY (transactionid)
+REFERENCES main.transaction (transactionid) MATCH SIMPLE;
