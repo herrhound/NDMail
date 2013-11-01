44fb049c76abc0d5334d440cb4f5e1e58dabd9fe
nikolatonkev <nikola_tonkev@yahoo.com>
2013-11-01 19:13:20 -0400

    template table altered, constraints added

--- Database/PostgreSQL/Scripts/main/template.sql
+++ Database/PostgreSQL/Scripts/main/template.sql
@@ -7,3 +7,16 @@ CREATE TABLE main.template
   systemstatusId INT             NOT NULL,
   transactionId  INT             NOT NULL
 );
+
+
+ALTER TABLE main.template
+ADD CONSTRAINT Fk_main_Template_ApplicationId FOREIGN KEY (applicationid)
+REFERENCES auth.application (applicationId) MATCH SIMPLE;
+
+ALTER TABLE main.template
+ADD CONSTRAINT Fk_main_Template_SystemStatusId FOREIGN KEY (systemstatusid)
+REFERENCES dict.systemstatus (systemstatusid) MATCH SIMPLE;
+
+ALTER TABLE main.template
+ADD CONSTRAINT Fk_main_Template_TransactionId FOREIGN KEY (transactionid)
+REFERENCES main.transaction (transactionid) MATCH SIMPLE;
