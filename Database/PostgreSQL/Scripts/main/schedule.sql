49fe5903d82bdc420ca370c7e0a0eebb2d3fa2b4
nikolatonkev <nikola_tonkev@yahoo.com>
2013-11-01 19:10:50 -0400

    schedule table altered, constraints added

--- Database/PostgreSQL/Scripts/main/schedule.sql
+++ Database/PostgreSQL/Scripts/main/schedule.sql
@@ -8,3 +8,15 @@ CREATE TABLE main.schedule
   systemstatusid INT                NOT NULL,
   transactionid  INT                NOT NULL
 );
+
+ALTER TABLE main.schedule
+ADD CONSTRAINT Fk_main_Schedule_ApplicationId FOREIGN KEY (applicationid)
+REFERENCES auth.application (applicationId) MATCH SIMPLE;
+
+ALTER TABLE main.schedule
+ADD CONSTRAINT Fk_main_Schedule_SystemStatusId FOREIGN KEY (systemstatusid)
+REFERENCES dict.systemstatus (systemstatusid) MATCH SIMPLE;
+
+ALTER TABLE main.schedule
+ADD CONSTRAINT Fk_main_Schedule_TransactionId FOREIGN KEY (transactionid)
+REFERENCES main.transaction (transactionid) MATCH SIMPLE;
--- Database/PostgreSQL/Scripts/main/task.sql
+++ Database/PostgreSQL/Scripts/main/task.sql
@@ -7,3 +7,24 @@ CREATE TABLE main.Task
   systemstatusid INT             NOT NULL,
   transactionid  INT             NOT NULL
 );
+
+
+ALTER TABLE main.task
+ADD CONSTRAINT Fk_main_Task_ApplicationId FOREIGN KEY (applicationid)
+REFERENCES auth.application (applicationId) MATCH SIMPLE;
+
+ALTER TABLE main.task
+ADD CONSTRAINT Fk_main_Task_SystemStatusId FOREIGN KEY (systemstatusid)
+REFERENCES dict.systemstatus (systemstatusid) MATCH SIMPLE;
+
+ALTER TABLE main.task
+ADD CONSTRAINT Fk_main_Task_TransactionId FOREIGN KEY (transactionid)
+REFERENCES main.transaction (transactionid) MATCH SIMPLE;
+
+ALTER TABLE main.task
+ADD CONSTRAINT Fk_main_Task_TemplateId FOREIGN KEY (templateid)
+REFERENCES main.template (templateid) MATCH SIMPLE;
+
+ALTER TABLE main.task
+ADD CONSTRAINT Fk_main_Task_ScheduleId FOREIGN KEY (scheduleid)
+REFERENCES main.schedule (scheduleid) MATCH SIMPLE;
