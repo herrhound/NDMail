3b884b542fba3d670f1f01feec63449dd7c2b8d4
Nikola Tonkev <nikola_tonkev@yahoo.com>
2013-11-05 23:15:28 -0500

    Bootstrap is here and main template changed

---
+++ app/controllers/Task.scala
@@ -0,0 +1,19 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Nikola
+ * Date: 11/5/13
+ * Time: 11:10 PM
+ * To change this template use File | Settings | File Templates.
+ */
+object Task extends Controller {
+
+  def index = Action {
+    Ok(views.html.Task.index("Your new application is ready."))
+  }
+
+}
